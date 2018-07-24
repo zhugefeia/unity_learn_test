336b5a1b50170ee886040edf9bd7d4f7354a5a79
yangshuaijie <yangshuaijie@bianfeng.com>
2018-07-24 17:30:54 +0800

    测试窗口

--- Assets/src/Test1.cs
+++ Assets/src/Test1.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class Test1 : MonoBehaviour {
 
     public Texture2D icon;
+    private string textFieldString = "text field";
 
-    void OnGUI ()
+    void OnGUI()
     {
-        GUI.Button(new Rect(10, 10, 100, 20), new GUIContent("Click me", icon, "This is the tooltip"));
+        if (GUI.Button(new Rect(10, 10, 100, 20), new GUIContent("Click me", icon, "This is the tooltip")))
+        {
+            Debug.Log("11");
+        }
         GUI.Label(new Rect(10, 40, 100, 20), GUI.tooltip);
+        if (GUI.RepeatButton(new Rect(10, 70, 100, 20), "Repeat"))
+        {
+            Debug.Log("2222");
+        }
+
+        textFieldString = GUI.TextField(new Rect(10, 100, 100, 20), textFieldString);
     }
 }
